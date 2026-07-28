034f4f97ece3922869e0c9d547d860c4027f67f2
ov4arenkoolexander <ov4arenko.olexander@gmail.com>
2026-07-28 23:12:38 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,38 +1,9 @@
 (function () {
     'use strict';
 
-    if (!window.Lampa) return;
-
-    Lampa.Listener.follow('app', function (event) {
-
-        if (event.type !== 'ready') return;
-
-        Lampa.Noty.show('Плагін Сані запущено 🚀');
-
-        Lampa.SettingsApi.addParam({
-            component: 'interface',
-            param: {
-                name: 'sania_test',
-                type: 'button'
-            },
-            field: {
-                name: '🎬 Плагін Сані'
-            },
-            onChange: function () {
-
-    Lampa.Modal.open({
-        title: '🎬 Плагін Сані',
-        html: `
-            <div style="padding:20px">
-                <h2>Вітаю!</h2>
-                <p>Це моє перше власне вікно в Lampa.</p>
-                <p>Незабаром тут буде список моїх фільмів.</p>
-            </div>
-        `
+    Lampa.Utils.putScriptAsync([
+        'https://ТВІЙ_GITHUB.github.io/LampaPlugin/main.js'
+    ], function () {
+        console.log('Sania Plugin loaded');
     });
-            }
-        });
-
-    });
-
 })();
